b75aa8d137ea24408645d02377b545f3730e04a1
sbdiv <143803108+sbdiv@users.noreply.github.com>
2024-02-15 09:11:32 +0200

    modified:   index.html 	modified:   main.py

--- main.py
+++ main.py
@@ -1,113 +1,125 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+import mimetypes
 import urllib.parse
 from pathlib import Path
-import mimetypes
-import logging
+from http.server import HTTPServer, BaseHTTPRequestHandler
 import socket
+import logging
+from urllib.parse import unquote_plus
+from datetime import datetime, timedelta
 import json
-from datetime import datetime
+from threading import Thread
 
-BASE_DIR = Path(__file__).resolve().parent
-BUFFER_SIZE = 1024
+BASE_DIR = Path()
+UDP_IP = '127.0.0.1'
+UDP_PORT = 5000
 HTTP_PORT = 3000
 HTTP_HOST = '0.0.0.0'
-SOCKET_HOST = '127.0.0.1'
-SOCKET_PORT = 5000
-DATA_FILE = BASE_DIR / 'storage' / 'data.json'
+MESSAGE = {}
 
-class RequestHandler(BaseHTTPRequestHandler):
+
+
+class GoitFramework(BaseHTTPRequestHandler):
+
     def do_GET(self):
-        parsed_url = urllib.parse.urlparse(self.path)
-        route_path = parsed_url.path
+        route = urllib.parse.urlparse(self.path)
+        print(route.query)
+        match route.path:
+            case '/':
+                self.send_html('index.html')
+            case '/message':
+                self.send_html('message.html')
+            case _:
+                file = BASE_DIR.joinpath(route.path[1:])
+                if file.exists():
+                    self.send_static(file)
+                else:
+                    self.send_html('error.html', 404)
 
-        if route_path == '/':
-            self.send_html_response('index.html')
-        elif route_path == '/message':
-            self.send_html_response('message.html')
-        else:
-            static_file_path = BASE_DIR.joinpath('static', route_path[1:])
-            if static_file_path.exists():
-                self.send_static_response(static_file_path)
-            else:
-                self.send_html_response('error.html', status_code=404)
+    def do_POST(self):
+        data = self.rfile.read(int(self.headers['Content-Length']))
+        print(data)
+        data_parse = urllib.parse.unquote_plus(data.decode())
+        print(data_parse)
+        data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
+        print(data_dict)
+        self.send_response(302)
+        self.send_header('Location', '/')
+        self.end_headers()
 
-    def send_html_response(self, filename, status_code=200):
+
+    def send_html(self, filename, status_code=200):
         self.send_response(status_code)
         self.send_header('Content-Type', 'text/html')
         self.end_headers()
-
         with open(filename, 'rb') as file:
             self.wfile.write(file.read())
 
-    def send_static_response(self, filename, status_code=200):
+    def send_static(self, filename, status_code=200):
         self.send_response(status_code)
-        mime_type, _ = mimetypes.guess_type(filename)
-
+        mime_type, *_ = mimetypes.guess_type(filename)
         if mime_type:
             self.send_header('Content-Type', mime_type)
         else:
             self.send_header('Content-Type', 'text/plain')
-
         self.end_headers()
-
         with open(filename, 'rb') as file:
             self.wfile.write(file.read())
 
-    def do_POST(self):
-        content_length = int(self.headers.get('Content-Length'))
-        post_data = self.rfile.read(content_length).decode('utf-8')
-        parsed_post_data = urllib.parse.parse_qs(post_data)
-
-        username = parsed_post_data.get('username', [''])[0]
-        message = parsed_post_data.get('message', [''])[0]
-
-        if username and message:
-            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-            entry = {timestamp: {"username": username, "message": message}}
-
-            with open(DATA_FILE, 'a+') as json_file:
-                json.dump(entry, json_file, indent=2)
-                json_file.write('\n')
-
-            socket_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            socket_client.sendto(post_data.encode('utf-8'), (SOCKET_HOST, SOCKET_PORT))
-            socket_client.close()
-
-            self.send_response(302)
-            self.send_header('Location', '/message')
-            self.end_headers()
-        else:
-            self.send_html_response('error.html', status_code=400)
-
-def run_http_server(host, port):
-    server_address = (host, port)
-    http_server = HTTPServer(server_address, RequestHandler)
-
-    logging.info("Starting HTTP server")
-    
+def save_data(data):
     try:
-        http_server.serve_forever()
+        parse_data = unquote_plus(data.decode())
+        parse_dict = {key: value for key, value in [el.split('=') for el in parse_data.split('&')]}
+        
+        current_time = str(datetime.now() + timedelta(hours=0))
+        MESSAGE[current_time] = parse_dict
+
+        with open('storage/data.json', 'w', encoding='utf-8') as file:
+            json.dump(MESSAGE, file, ensure_ascii=False, indent=4)
+            file.write('\n')
+
+    except ValueError as error:
+        logging.error(f'ValueError in save_data_from_form: {error}')
+    except OSError as error:
+        logging.error(f'OSError in save_data_from_form: {error}')
+
+
+
+def run_server(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server = ip, port
+    sock.bind(server)
+    try:
+        while True:
+            msg, address = sock.recvfrom(1024)
+            logging.info(f"Socket receiver {address}: {msg}")
+            save_data(msg)
+
+    except KeyboardInterrupt:
+        print(f'Destroy server')
+    finally:
+        sock.close()
+
+def run_client(ip, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind((ip, port))
+    logging.info("Starting socket server")
+    try:
+        while True:
+            msg, address = server_socket.recvfrom(1024)
+            logging.info(f"Socket receiver {address}: {msg}")
+            save_data(msg)
     except KeyboardInterrupt:
         pass
     finally:
-        http_server.server_close()
+        server_socket.close()
+
+
+
+
 
 if __name__ == '__main__':
-    DATA_FILE.parent.mkdir(parents=True, exist_ok=True)
+    server = Thread(target=run_server, args=(HTTP_HOST, HTTP_PORT))
+    server.start()
 
-    import threading
-
-    http_thread = threading.Thread(target=run_http_server, args=(HTTP_HOST, HTTP_PORT))
-    http_thread.start()
-
-
-    socket_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    socket_server.bind((SOCKET_HOST, SOCKET_PORT))
-
-    logging.info("Starting socket server")
-
-    while True:
-        data, addr = socket_server.recvfrom(BUFFER_SIZE)
-        logging.info(f"Received data from {addr}: {data.decode('utf-8')}")
-
-    socket_server.close()
+    server_socket = Thread(target=run_client, args=(UDP_IP, UDP_PORT))
+    server_socket.start()
